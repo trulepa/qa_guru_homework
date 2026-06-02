18ea1c13d4f4d6536dc37e36aa59b97b971e200a
e.khomiakov <trulepa@mail.ru>
2026-06-03 00:17:12 +0300

    update: test_form change fio and email

---
+++ 3_Selenium/hw_2/variables.py
@@ -0,0 +1,14 @@
+url = "https://qa-guru.github.io/one-page-form/text-box.html"
+# driver =  webdriver.Chrome()
+locator = {
+    "full_name_locator": "userName",
+    "email_locator": "userEmail",
+    "submit_button_locator": "submit",
+    "result_box_locator": "output"
+    }
+result_line_name = {
+    "full_name": "Name:",
+    "Email": "Email:",
+    "current_address": "Current Address :",
+    "permananet_address": "Permananet Address :"
+}
